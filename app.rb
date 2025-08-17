753b347f53b1bb53104fe7fbfeddc1ca87ce6229
Jacob Harris <mail@jacobharr.is>
2025-08-17 16:20:25 -0400

    HTMX is working!

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'kramdown'
 
 require_relative 'lib/models'
+require_relative 'lib/actions/create_post'
 require_relative 'lib/actions/vouch_post'
 
 get '/' do
@@ -14,6 +15,17 @@ get '/timeline/:username' do |username|
 
   posts = Post.order_by(Sequel.desc(:created_at)).all
   erb :timeline, locals: { posts: posts, current_user: user }
+end
+
+post '/actions/post' do
+  content = params[:content]
+  username = params[:username]
+  user = User.find_username(username)
+
+  Actions::CreatePost.new(username: username, text: content).run
+
+  posts = Post.order_by(Sequel.desc(:created_at)).all
+  erb :_posts_timeline, locals: { posts: posts, current_user: user }
 end
 
 post '/actions/vouch/:id' do |id|
